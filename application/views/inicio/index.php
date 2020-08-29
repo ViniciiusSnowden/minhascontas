05a912fb6536ffdd64df9c48feabf9c55686fdb1
yagobrandao20 <yagobrandao20@gmail.com>
2020-08-29 17:51:15 -0300

    ok

--- application/views/inicio/index.php
+++ application/views/inicio/index.php
@@ -4,9 +4,9 @@
      <div class="d-sm-flex align-items-center justify-content-between mb-4">
          <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
      </div>
-     <div class="row animate__animated animate__jackInTheBox">
+     <div class="row">
          <!-- Earnings (Monthly) Card Example -->
-         <div class="col-xl-3 col-md-6 mb-4">
+         <div class="col-xl-3 col-md-6 mb-4 animate__animated animate__backInLeft">
              <div class="card border-left-primary shadow h-100 py-2">
                  <div class="card-body">
                      <div class="row no-gutters align-items-center">
@@ -23,7 +23,7 @@
          </div>
 
          <!-- Earnings (Monthly) Card Example -->
-         <div class="col-xl-3 col-md-6 mb-4">
+         <div class="col-xl-3 col-md-6 mb-4 animate__animated animate__backInRight">
              <div class="card border-left-success shadow h-100 py-2">
                  <div class="card-body">
                      <div class="row no-gutters align-items-center">
@@ -40,7 +40,7 @@
          </div>
 
          <!-- Earnings (Monthly) Card Example -->
-         <div class="col-xl-3 col-md-6 mb-4">
+         <div class="col-xl-3 col-md-6 mb-4 animate__animated animate__backInLeft">
              <div class="card border-left-info shadow h-100 py-2">
                  <div class="card-body">
                      <div class="row no-gutters align-items-center">
@@ -65,7 +65,7 @@
          </div>
 
          <!-- Pending Requests Card Example -->
-         <div class="col-xl-3 col-md-6 mb-4">
+         <div class="col-xl-3 col-md-6 mb-4 animate__animated animate__backInRight">
              <div class="card border-left-warning shadow h-100 py-2">
                  <div class="card-body">
                      <div class="row no-gutters align-items-center">
