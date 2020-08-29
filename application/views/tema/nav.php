4c7929517bd6aad23a5674f6eac8fc837768c05c
yagobrandao20 <yagobrandao20@gmail.com>
2020-08-29 16:23:47 -0300

    ok

--- application/views/tema/nav.php
+++ application/views/tema/nav.php
@@ -109,10 +109,10 @@
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
+                <!-- <a class="dropdown-item" href="#">
                   <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                   Perfil
-                </a>
+                </a> -->
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="/logout">
                   <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
