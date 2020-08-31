ab30c8893f07af45aeda141dff95e26e97ab9a0d
yagobrandao20 <yagobrandao20@gmail.com>
2020-08-31 09:24:23 -0300

    ok

--- application/config/config.php
+++ application/config/config.php
@@ -379,7 +379,7 @@ $config['encryption_key'] = '';
 */
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
+$config['sess_expiration'] = 60*60*24*365;
 $config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
